03984d9714e6a10ab926afd9987de8029ef29732
soriadelva <soria.delva@gmail.com>
2024-10-25 10:10:25 +0200

    Store a helper function that calculates the number of decimal places in any given number

---
+++ src/helper_functions.R
@@ -0,0 +1,10 @@
+#This function calculates the number of decimal places in any given numeric value 
+# eg., 15.21 has 2 decimal places, 15.2569 has 4 decimal places, 15.25690 also has 4, as 0 in the end doesn't count
+decimalplaces <- function(x) {
+  if (abs(x - round(x)) > .Machine$double.eps^0.5) {
+    nchar(strsplit(sub('0+$', '', as.character(x)), ".", fixed = TRUE)[[1]][[2]])
+  } else {
+    return(0)
+  }
+}
+
