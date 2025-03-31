160068c3d77e0f6d2f52d0b2286717fed7355618
soriadelva <soria.delva@gmail.com>
2025-03-31 18:31:26 +0200

    Load packages in script 00

--- src/00_prepare_files_and_folders.R
+++ src/00_prepare_files_and_folders.R
@@ -1,3 +1,14 @@
 #--------------------------------------------
 #-----------  Load packages  ----------------
 #--------------------------------------------
+packages <- c("curl"
+)
+
+for(package in packages) {
+  print(package)
+  if( ! package %in% rownames(installed.packages()) ) { install.packages( package ) }
+  library(package, character.only = TRUE)
+}
+
+
+
