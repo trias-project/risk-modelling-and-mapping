f3f16f6e112449c1a13eb9d7e320dc5165de0e53
soriadelva <soria.delva@gmail.com>
2025-04-03 15:50:22 +0200

    Store and create future climate layers for Belgium (global model)

--- src/00_prepare_files_and_folders.R
+++ src/00_prepare_files_and_folders.R
@@ -208,3 +208,112 @@ download_zenodo(doi="https://doi.org/10.5281/zenodo.15102496",
                 quiet=FALSE)
 
 
+#---------------------------------------------------------
+#---------- Store future climate layers (CHELSA) ---------
+#---------------------------------------------------------
+#Store future CHELSA layers at global level
+options(timeout = 600) #set time-out to 10 min 
+
+for(i in c("01", "04", "05", "06","07", "12","13","14","15")){
+  
+  # Remove leading zeros during download
+  i_download <- as.integer(i)
+  
+  # Define CHELSA layer name
+  layer_name <- switch(i,
+                       "01" = "meantemp",
+                       "04" = "temp_seasonality",
+                       "05" = "maxTmpWarmestMon",
+                       "06"= "minTmpColdestMon",
+                       "07"="temp_annRange",
+                       "12"="annPrecip",
+                       "13"="precipWettestMon",
+                       "14"="precipDriestMon",
+                       "15"="precipSeasonality")
+  
+  if(grepl("windows", Sys.getenv("OS"), ignore.case = TRUE)) {
+    download.file(url = paste0("https://os.zhdk.cloud.switch.ch/chelsav1/cmip5/2041-2060/bio/CHELSA_bio_mon_MPI-ESM-LR_rcp26_r1i1p1_g025.nc_",i_download,"_2041-2060_V1.2.tif"),
+                  mode = "wb",
+                  destfile = file.path(rcp26_globalmodel_folder,paste0("CHELSA_",layer_name,"_",i,".tif")))
+  }else{
+    download.file(url = paste0("https://os.zhdk.cloud.switch.ch/chelsav1/cmip5/2041-2060/bio/CHELSA_bio_mon_MPI-ESM-LR_rcp26_r1i1p1_g025.nc_",i_download,"_2041-2060_V1.2.tif"),
+                  destfile = file.path(rcp26_globalmodel_folder,paste0("CHELSA_",layer_name,"_",i,".tif")))
+  }
+  
+  if(grepl("windows", Sys.getenv("OS"), ignore.case = TRUE)) {
+    download.file(url = paste0("https://os.zhdk.cloud.switch.ch/chelsav1/cmip5/2041-2060/bio/CHELSA_bio_mon_MPI-ESM-LR_rcp45_r1i1p1_g025.nc_",i_download,"_2041-2060_V1.2.tif"),
+                  mode = "wb",
+                  destfile = file.path(rcp45_globalmodel_folder,paste0("CHELSA_",layer_name,"_",i,".tif")))
+  }else{
+    download.file(url = paste0("https://os.zhdk.cloud.switch.ch/chelsav1/cmip5/2041-2060/bio/CHELSA_bio_mon_MPI-ESM-LR_rcp45_r1i1p1_g025.nc_",i_download,"_2041-2060_V1.2.tif"),
+                  destfile = file.path(rcp45_globalmodel_folder,paste0("CHELSA_",layer_name,"_",i,".tif")))
+  }
+  
+  if(grepl("windows", Sys.getenv("OS"), ignore.case = TRUE)) {
+    download.file(url = paste0("https://os.zhdk.cloud.switch.ch/chelsav1/cmip5/2041-2060/bio/CHELSA_bio_mon_MPI-ESM-LR_rcp85_r1i1p1_g025.nc_",i_download,"_2041-2060_V1.2.tif"),
+                  mode = "wb",
+                  destfile = file.path(rcp85_globalmodel_folder,paste0("CHELSA_",layer_name,"_",i,".tif")))
+  }else{
+    download.file(url = paste0("https://os.zhdk.cloud.switch.ch/chelsav1/cmip5/2041-2060/bio/CHELSA_bio_mon_MPI-ESM-LR_rcp85_r1i1p1_g025.nc_",i_download,"_2041-2060_V1.2.tif"),
+                  destfile = file.path(rcp85_globalmodel_folder,paste0("CHELSA_",layer_name,"_",i,".tif")))
+  }
+}
+
+
+#---------------------------------------------------------
+#------Create future climate layers for Belgium  ---------
+#---------------------------------------------------------
+#Crop and mask layers to Belgium, this may take some time!
+#If you'd like to predict for another country, change the country name
+belgium<-rnaturalearth::ne_countries(country="Belgium", scale=10)[1]
+belgium_ext<-terra::ext(belgium) 
+belgium_vector <- terra::vect(belgium) #Convert to a SpatVector, used for masking
+
+#List files
+be26 <- list.files(here("./data/external/climate/Global_finalRCP/rcp26"), pattern = 'tif', full.names = TRUE)
+be45 <- list.files(here("./data/external/climate/Global_finalRCP/rcp45"), pattern = 'tif', full.names = TRUE)
+be85 <- list.files(here("./data/external/climate/Global_finalRCP/rcp85"), pattern = 'tif', full.names = TRUE)
+
+# List scenarios to iterate through
+set.seed(123)
+list_names <- c("be26", "be45", "be85")
+
+# Iterate over the list names
+for (list_name in list_names) {
+  # get the list (be26, be45, and be85)
+  current_list <- get(list_name)
+  
+  fullstack <- lapply(current_list, function(f) {
+    r <- rast(f)
+    r[r==-32768]<-NA #Set marine pixels to NA
+    r <- crop(r, belgium_ext)
+    r <- mask(r, belgium_vector)
+    r<- r/10 #Divide by 10 to comply with predictors in global model framework
+    return(r)
+  })
+  
+  # Combine 
+  fullstack <- do.call(c, fullstack)  
+  
+  # Assign the processed fullstack to a variable dynamically (e.g., fullstack26, fullstack45, fullstack85)
+  assign(paste0("fullstack", substr(list_name, 3, 4)), fullstack)
+}
+
+#--------------- Export rasters -------------
+
+#RCP 2.6 
+for (i in 1:nlyr(fullstack26)) {
+  writeRaster(fullstack26[[i]], filename = here(rcp26_belgium_globalmodel_folder, paste0(names(fullstack26[[i]]), ".tif")), overwrite=TRUE)
+}
+
+#RCP 4.5
+for (i in 1:nlyr(fullstack45)) {
+  writeRaster(fullstack45[[i]], filename = here(rcp45_belgium_globalmodel_folder, paste0(names(fullstack45[[i]]), ".tif")), overwrite=TRUE)
+}
+
+#RCP 8.5
+for (i in 1:nlyr(fullstack85)) {
+  writeRaster(fullstack85[[i]], filename = here(("./data/external/climate/Global_finalRCP/belgium_rcps/rcp85"), paste0(names(fullstack85[[i]]), ".tif")), overwrite=TRUE)
+}
+
+
