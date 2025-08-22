218b32d4ee7505dcf4d41717f481d11530ec115c
soriadelva <soria.delva@gmail.com>
2025-08-22 14:34:29 +0200

    Define favourability transformation function

--- src/02_fit_global_model.R
+++ src/02_fit_global_model.R
@@ -501,8 +501,15 @@ with_progress({
     #--------------------------------------------
     #--- Run multiple machine learning models ---
     #--------------------------------------------
-    #Divide all climate data by 10
-    global.data.df.uncor<-cbind("species"=  global.data.df.subset$species,divide10(global.data.df.subset[,-c(1)]))
+    #Define the favourability transformation function
+    favourability_from_prob <- function(prob_raster, prev_ratio) {
+      odds <- prob_raster / (1 - prob_raster)
+      fav <- odds / (prev_ratio + odds)
+      fav[is.infinite(fav)] <- NA
+      fav[fav < 0] <- 0
+      fav[fav > 1] <- 1
+      return(fav)
+    }
     
     
     #--------------------------------------------
