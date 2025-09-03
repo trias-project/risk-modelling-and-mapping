16bcafbdafc5cc741fa8f182cc63eb70f7226ba5
DiederikStrubbe <diederik.strubbe@gmail.com>
2025-09-03 16:27:25 +0200

    Setup CV script
    just created a file to start

---
+++ src/X_NewSCriptCV.R
@@ -0,0 +1,5 @@
+###  Evaluate the performance of each the EU level ensemble models using independent data set from the future 
+#####################################################################
+
+
+#CVSCRIOT
