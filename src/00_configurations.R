6d84360df87011ec7e552df2f002e3ed9756fb91
soriadelva <soria.delva@gmail.com>
2025-09-24 11:24:01 +0200

    Add mtp probabilities to configurations file

--- src/00_configurations.R
+++ src/00_configurations.R
@@ -10,3 +10,5 @@ country_of_interest <- "Belgium"
 occurrence_thinning_method <- "kmeans_clustering" #either "random" or "kmeans_clustering"
 
 n_clusters <- 10000 #Only relevant when occurrence_thinning_method is "kmeans_clustering"
+
+mtp_probabilities <- c(0.01, 0.05) #Define MTP threshold (0.01 = 1%; 0.05 = 5%,...)
