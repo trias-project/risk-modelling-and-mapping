1ffd0b9884bd9190a6542718fe7641825b23a772
joao_goncalves <joaofgo@gmail.com>
2026-04-24 13:07:03 +0100

    Adds custom EU bound layer

--- src/00_configurations.R
+++ src/00_configurations.R
@@ -11,6 +11,8 @@ pseudoabsence_thinning_method <- "kmeans_clustering" #either "random" or "kmeans
 
 mtp_probabilities <- c(0.01, 0.05) #Define MTP thresholds (0.01 = 1%; 0.05 = 5%,...)
 
+custom_eu_boundary_path <- NULL  #either NULL or a path to a custom Europe boundary vector layer
+
 custom_country_boundary_path <- NULL  #either NULL or a path to a shapefile, if not NULL this overwrites country_of_interest
 
 country_of_interest <- "Europe"
